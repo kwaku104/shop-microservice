b89b671c541b862f89c5f6861a615c8538073a72
Kwaku Appiah-Adu <kwakuappiahadu@gmail.com>
2023-03-14 13:09:42 +0000

    method to find Inventories by skuCode

--- inventory-service/src/main/java/com/shop/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/shop/inventoryservice/repository/InventoryRepository.java
@@ -2,10 +2,9 @@ package com.shop.inventoryservice.repository;
 
 import com.shop.inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
-    Optional<Inventory> findBySkuCode(String skuCode);
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
 }
