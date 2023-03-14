7459a2b246824e635cc57d8ea05eeae6244c5ad0
Kwaku Appiah-Adu <kwakuappiahadu@gmail.com>
2023-03-14 13:11:20 +0000

    return list of products in stock from inventory responses by skuCode

--- inventory-service/src/main/java/com/shop/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/shop/inventoryservice/service/InventoryService.java
@@ -1,8 +1,11 @@
 package com.shop.inventoryservice.service;
 
+import com.shop.inventoryservice.dto.InventoryResponse;
 import com.shop.inventoryservice.repository.InventoryRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class InventoryService {
@@ -14,8 +17,14 @@ public class InventoryService {
     }
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode) {
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory ->
+                    InventoryResponse.builder()
+                            .skuCode(inventory.getSkuCode())
+                            .isInStock(inventory.getQuantity() > 0)
+                            .build()
+                ).toList();
     }
 
 }
