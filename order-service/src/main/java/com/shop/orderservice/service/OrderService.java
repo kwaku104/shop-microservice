70466404b9404ce13a32af605b3f7b67f1fc9ede
Kwaku Appiah-Adu <kwakuappiahadu@gmail.com>
2023-03-16 22:33:07 +0000

    made inventory service multi az and made enabled loadbalancing on webclient bean

--- order-service/src/main/java/com/shop/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/shop/orderservice/service/OrderService.java
@@ -19,11 +19,11 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
-    public OrderService(OrderRepository orderRepository, WebClient webClient) {
+    public OrderService(OrderRepository orderRepository, WebClient.Builder webClientBuilder) {
         this.orderRepository = orderRepository;
-        this.webClient = webClient;
+        this.webClientBuilder = webClientBuilder;
     }
 
     public void placeOrder(OrderRequest orderRequest) {
@@ -42,8 +42,8 @@ public class OrderService {
                 .toList();
 
         //Call inventory and check if product is in stock
-        InventoryResponse[] inventoryResponses = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class) // type of response we are returning from inventory service
