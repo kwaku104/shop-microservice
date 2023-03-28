254ee34eac490db0809fd7fca8aaaf52839634d8
Kwaku Appiah-Adu <kwakuappiahadu@gmail.com>
2023-03-29 00:06:38 +0100

    add circuit breaker implementation and fallback method for when it is in open state

--- order-service/src/main/java/com/shop/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/shop/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.shop.orderservice.controller;
 
 import com.shop.orderservice.dto.OrderRequest;
 import com.shop.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,8 +18,13 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order Placed Successfully";
+    }
+
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return "Oops! Something went wrong, please order after sometime!";
     }
 }
