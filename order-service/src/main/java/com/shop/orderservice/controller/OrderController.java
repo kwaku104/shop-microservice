fc4523392d9841926e86862957f4c58fb11372bf
Kwaku Appiah-Adu <kwakuappiahadu@gmail.com>
2023-03-31 00:24:04 +0100

    Added retry functionality to order-service

--- order-service/src/main/java/com/shop/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/shop/orderservice/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.shop.orderservice.controller;
 import com.shop.orderservice.dto.OrderRequest;
 import com.shop.orderservice.service.OrderService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -23,6 +24,7 @@ public class OrderController {
     @ResponseStatus(HttpStatus.CREATED)
     @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     @TimeLimiter(name= "inventory")
+    @Retry(name = "inventory")
     public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
         return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
     }
