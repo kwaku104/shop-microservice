14fea997bb0fd20d5c453f5e77a08e3686428219
Kwaku Appiah-Adu <kwakuappiahadu@gmail.com>
2023-04-11 19:30:40 +0100

    created span id for distributed tracing and added kafka template to send message to topic

--- order-service/src/main/java/com/shop/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/shop/orderservice/service/OrderService.java
@@ -3,9 +3,14 @@ package com.shop.orderservice.service;
 import com.shop.orderservice.dto.InventoryResponse;
 import com.shop.orderservice.dto.OrderLineItemsDto;
 import com.shop.orderservice.dto.OrderRequest;
+import com.shop.orderservice.event.OrderPlacedEvent;
 import com.shop.orderservice.model.Order;
 import com.shop.orderservice.model.OrderLineItems;
 import com.shop.orderservice.repository.OrderRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -16,14 +21,19 @@ import java.util.UUID;
 
 @Service
 @Transactional
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
+    private final Tracer tracer;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
-    public OrderService(OrderRepository orderRepository, WebClient.Builder webClientBuilder) {
+    public OrderService(OrderRepository orderRepository, WebClient.Builder webClientBuilder, Tracer tracer, KafkaTemplate kafkaTemplate) {
         this.orderRepository = orderRepository;
         this.webClientBuilder = webClientBuilder;
+        this.tracer = tracer;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
     public String placeOrder(OrderRequest orderRequest) {
@@ -41,21 +51,33 @@ public class OrderService {
                 .map(OrderLineItems::getSkuCode)
                 .toList();
 
-        //Call inventory and check if product is in stock
-        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
-                .uri("http://inventory-service/api/inventory",
-                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
-                .retrieve()
-                .bodyToMono(InventoryResponse[].class) // type of response we are returning from inventory service
-                .block(); // by default webclient makes asynchronous requests so .block() will cause WbCl to make synchronous request
+        log.info("Calling inventory service");
 
-        boolean allProductsInStock = Arrays.stream(inventoryResponses).allMatch(InventoryResponse::isInStock);
+        // Creating span ID
+        Span inventoryServiceLookup = tracer.nextSpan().name("InventoryServiceLookup");
 
-        if(allProductsInStock) {
-            orderRepository.save(order);
-            return "Order Placed Successfully";
-        } else {
-            throw new IllegalArgumentException("Product is not in stock please try again later");
+        try(Tracer.SpanInScope spanInScope = tracer.withSpan(inventoryServiceLookup.start())) {
+            // Assign span id to this code block
+            //Call inventory and check if product is in stock
+            InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                    .uri("http://inventory-service/api/inventory",
+                            uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                    .retrieve()
+                    .bodyToMono(InventoryResponse[].class) // type of response we are returning from inventory service
+                    .block(); // by default webclient makes asynchronous requests so .block() will cause WbCl to make synchronous request
+
+            boolean allProductsInStock = Arrays.stream(inventoryResponses).allMatch(InventoryResponse::isInStock);
+
+            if(allProductsInStock) {
+                orderRepository.save(order);
+                // Making call to kafka cluster when order is placed
+                kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
+                return "Order Placed Successfully";
+            } else {
+                throw new IllegalArgumentException("Product is not in stock please try again later");
+            }
+        } finally {
+            inventoryServiceLookup.end();
         }
     }
 
